42b162a1e07ba6b170aff0aa89b76d5a3cf2f34a
anand kumar tiwari <aktiwari@nextgen.com>
2020-05-21 09:21:06 +0530

    Using make method to create map

--- Map-Example.go
+++ Map-Example.go
@@ -2,13 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-	 var mymap = map[string]int{
-		"Anand":20,
-		"Kumar":33,
+func createMapUsingMake() {
+	var m = make(map[string]string)
+	m["name"] = "Anand"
+	m["address"] = "Ranchi"
+	for k, v := range m {
+		fmt.Println(k + "\t" + v)
 	}
-	for k,v :=range mymap {
-		fmt.Println(k, "\t" ,v)
+}
+func main() {
+	var mymap = map[string]int{
+		"Anand": 20,
+		"Kumar": 33,
+	}
+	for k, v := range mymap {
+		fmt.Println(k, "\t", v)
 	}
 	fmt.Println(mymap["Anand"])
+
+	createMapUsingMake()
 }
